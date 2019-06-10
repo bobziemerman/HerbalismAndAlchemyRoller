1bd10df9b53aeee6bd413db15e554a964b39b61b
bobziemerman <bobzim17@gmail.com>
2019-06-10 18:48:48 -0500

    final v1 of the herbalism roller

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ angular.module('todoApp', []).controller('TodoListController', function($scope) 
   $scope.weathers = [
     "Day",
     "Night",
-    "Raining"
+    "Raining",
+    "In a cave"
   ]
 
   $scope.environments = [
@@ -32,23 +33,73 @@ angular.module('todoApp', []).controller('TodoListController', function($scope) 
   function d6(){
     return Math.floor(Math.random() *6) +1
   }
+  function twoD6(){
+    return (d6()+d6())
+  }
   function d20(){
     return Math.floor(Math.random() *20) +1
+  }
+  function d100(){
+    return Math.floor(Math.random() *100) +1
   }
 
 
   $scope.harvest = function(){
-    var attempt = d20() + $scope.mod
-    if(attempt < 15){
-      $scope.rolls.push("No useful ingredients found today (Roll: "+attempt+")")
+    var attempt = d20()
+    if(attempt + $scope.mod < 15){
+      $scope.rolls.push("No useful ingredients found today (Roll: "+attempt+"+"+$scope.mod+"="+(attempt+$scope.mod)+")")
     } else {
-      roll()
+      var numIngredients = d4()
+      $scope.rolls.push("You found "+numIngredients+" ingredients!")
+      for(var i=0; i<numIngredients; i++){
+        roll()
+      }
     }
   }
   
   function roll(){
+    var rollValue = (twoD6() -2)
+    if(rollValue < 4 || rollValue > 8){
+      if(!elementalWater()){
+        handleIngredient(ingredients[$scope.environment][rollValue])
+      }
+    } else {
+      handleIngredient(ingredients[$scope.environment][rollValue])
+    }
+  }
 
-    $scope.rolls.push("yay")
+  function elementalWater() {
+    var val = d100()
+    if (val >= 75){
+      $scope.rolls.push("--Elemental Water")
+      return true
+    } else {
+      return false
+    }
+  }
+
+  function handleIngredient(ingredient) {
+    switch(ingredient.rule) {
+      case 'common':
+        handleIngredient(ingredients["Common"][twoD6()-2])
+        break;
+      case 'night':
+        if($scope.weather === "Night"){
+          $scope.rolls.push("--"+ingredient.name)
+        } else {
+          handleIngredient(ingredients[$scope.environment][twoD6()-2])
+        }
+        break;
+      case 'cave':
+        $scope.rolls.push(($scope.weather === "In a cave" ? "--2 ":"--")+ingredient.name)
+        break;
+      case 'rain':
+        $scope.rolls.push(($scope.weather === "Raining" ? "--2 ":"--")+ingredient.name)
+        break;
+      default:
+        $scope.rolls.push("--"+ingredient.name)
+        break;
+    }
   }
 
 });
